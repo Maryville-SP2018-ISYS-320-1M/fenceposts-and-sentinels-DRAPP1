70784b7f51e4fe293abff9046a627e5bff84027d
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-04-19 15:44:13 -0500

    g

--- src/P2_InfiniteGuessingGame.java
+++ src/P2_InfiniteGuessingGame.java
@@ -1,7 +1,7 @@
 
 
 /*
-	ISYS 320
+	ISYS 320 DONE DONE DONE DONE DONE
 	Name(s): Dave Rapp
 	Date: 4-18-18
 	
@@ -14,29 +14,82 @@
 
 import java.util.Random;
 import java.util.Scanner;
-public class P2_InfiniteGuessingGame
-{
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        Random random = new Random();
-        long from = 1;
-        long to = 100;
-        long randomNumber = random.nextInt() + from;
-        int guessedNumber = 0;
- 
-        System.out.printf("The number is between %d and %d.\n", from, to);
- 
-        do
-        {
-            System.out.print("Guess what the number is: ");
-            guessedNumber = scan.nextInt();
-            if (guessedNumber < randomNumber)
-                System.out.println("Your guess is too high!");
-            else if (guessedNumber > randomNumber)
-                System.out.println("Your guess is too low!");
-            else
-                System.out.println("You got it!");
-        } while (guessedNumber != randomNumber);
-    }
+
+public class P2_InfiniteGuessingGame {
+
+	public static void main(String[] args) {
+		int secretNumber = getSecretNumber();
+		int numberOfGuessesAllowed = 7;
+
+		outputWelcomeMessage();
+		int guessesTaken = playGame(secretNumber, numberOfGuessesAllowed);
+		outputEndingMessage(guessesTaken, numberOfGuessesAllowed, secretNumber);
+	}
+
+	public static void outputWelcomeMessage() {
+		System.out.println("This is a guessing game.");
+		System.out.println("I picked a random number from 1 to 100");
+	}
+
+	public static void outputEndingMessage(int guessesTaken, int numberOfGuessesAllowed, int secretNumber) {
+		if (guessesTaken < numberOfGuessesAllowed) {
+			System.out.println("Congratuations, you guessed it!");
+		} 
+		else if (guessesTaken  < secretNumber) {
+            System.out.println("Your guess is too low!");
+            }
+		
+		else {
+			System.out.println("The secret number was " + secretNumber + ". Try again.");
+		}
+		if (guessesTaken > secretNumber) {
+            System.out.println("Your guess is too high!");
+            }
+      
+	}
+
+	public static int getSecretNumber() {
+		Random generator = new Random();
+		return generator.nextInt(100) + 1;
+	}
+
+	public static int getNextGuess() {
+		Scanner console = new Scanner(System.in);
+		int nextGuess;
+		do {
+			System.out.println("What is your guess? ");
+			nextGuess = console.nextInt();
+
+			if (nextGuess < 0 || nextGuess > 100) {
+				System.out.println("ERROR: Guess should be between 0 and 100");
+			}
+		} while (nextGuess < 0 || nextGuess > 100);
+
+		
+
+		return nextGuess;
+	}
+
+	public static int playGame(int secretNumber, int allowedGuesses) {
+		int numOfGuessesTaken = 0;
+		while (numOfGuessesTaken < allowedGuesses) {
+			int nextGuess = getNextGuess();
+
+			if (nextGuess == secretNumber) {
+				return numOfGuessesTaken;
+			} else {
+				System.out.println("Nope, guess again. ");
+				if(nextGuess < secretNumber) {
+					System.out.println("Your Guess is too low ");
+				}
+				if(nextGuess > secretNumber) {
+					System.out.println("Your Guess is too high");
+				}
+				numOfGuessesTaken++;
+			}
+		}
+
+		return numOfGuessesTaken;
+	}
+
 }
